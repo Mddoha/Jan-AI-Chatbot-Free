ec3be812c70843dfee4b95876e6e5b0082ca655e
Mddoha <167410283+Mddoha@users.noreply.github.com>
2025-07-23 05:30:41 +0600

    Update telegram.py

--- api/telegram.py
+++ api/telegram.py
@@ -4,7 +4,7 @@ import json
 
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
+        content_length = int(self.headers.get('Content-Length', 0))
         post_data = self.rfile.read(content_length)
 
         print("Telegram Webhook Hit:", post_data.decode())
